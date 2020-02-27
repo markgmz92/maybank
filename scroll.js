f81eef901125e98352a5bde2fa3d392551b1991d
markgmz92 <markgmz92@gmail.com>
2020-02-27 21:54:16 +0800

    Adjusted scroll time

--- scroll.js
+++ scroll.js
@@ -2,13 +2,13 @@ setInterval(function scroll() {
     $("div").each(function(i, e) {
       $("html, body").animate({
         scrollTop: $(e).offset().top
-      }, 10000).delay(8000); // First value is a speed of scroll, and second time break
+      }, 9500).delay(10000); // First value is a speed of scroll, and second time break
     });
   
     setTimeout(function() {
       $('html, body').animate({
         scrollTop: 0
-      }, 10000); // This is the speed of scroll up
-    }, 10000); //This means after what time should it begin (after scrolling ends)
+      }, 9000); // This is the speed of scroll up
+    }, 2000); //This means after what time should it begin (after scrolling ends)
     return scroll;
-  }(), 10000);
+  }(),10000);
