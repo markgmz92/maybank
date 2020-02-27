a96c2ff926e7c8ae51172c1a030ab15708ad8748
markgmz92 <markgmz92@gmail.com>
2020-02-28 04:36:45 +0800

    Code clean up

--- scroll.js
+++ scroll.js
@@ -2,13 +2,14 @@ setInterval(function scroll() {
     $("div").each(function(i, e) {
       $("html, body").animate({
         scrollTop: $(e).offset().top
-      }, 9500).delay(500); // First value is a speed of scroll, and second time break
+      }, 5000).delay(500); // First value is a speed of scroll, and second time break
     });
   
     setTimeout(function() {
       $('html, body').animate({
         scrollTop: 0
-      }, 9000); // This is the speed of scroll up
+      }, 5000); // This is the speed of scroll up
     }, 2000); //This means after what time should it begin (after scrolling ends)
     return scroll;
-  }(),10000);
+  }(),1000); //This value means after what time should the function be triggered again
+  //(It should be sum of all animation time and delay) 9000 = 5000 + 4000
