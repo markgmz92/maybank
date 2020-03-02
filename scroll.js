e8eb072bd10ae8c6311ef16782aa7ba4aeaba702
markgmz92 <markgmz92@gmail.com>
2020-03-03 03:20:40 +0800

    Added auto scroll

--- scroll.js
+++ scroll.js
@@ -1,17 +1,10 @@
-setInterval(function scroll() {
-    $("div").each(function(i, e) {
-      $("html, body").animate({
-        scrollTop: $(e).offset().top
-      }, 5000).delay(500); // First value is a speed of scroll, and second time break
-    });
-  
-    setTimeout(function() {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 5000); // This is the speed of scroll up
-    }, 2000); //This means after what time should it begin (after scrolling ends)
-    return scroll;
-  }(),1000); //This value means after what time should the function be triggered again
-  //(It should be sum of all animation time and delay) 9000 = 5000 + 4000
+$(document).ready(function() { 
+    $("button").click(function() { 
+        $("html, body").animate({ 
+            scrollTop: $( 
+              'html, body').get(0).scrollHeight 
+        }, 5000); 
+        
+    }); 
 
-  
+}); 
